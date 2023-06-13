6be1950fedef2bccad8692bc2848088766b0679c
thaikt123 <136434116+thaikt123@users.noreply.github.com>
2023-06-13 15:44:41 +0700

    2

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php
-$server_username = "root"; // thông tin đăng nhập host
-$server_password = ""; // mật khẩu, trong trường hợp này là trống
-$server_host = "localhost"; // host là localhost
+$server_username = "dbmasteruser"; // thông tin đăng nhập host
+$server_password = "i1;BF~wH#U(O5_c3HDqk]5}=h*d-B)my"; // mật khẩu, trong trường hợp này là trống
+$server_host = "ls-949ad5ea5968f5d8446ca7148dc3f318c89cfdd4.coiqqpvdjaju.ap-south-1.rds.amazonaws.com"; // host là localhost
 $database = 'gadatacontrol'; // 
 
 // Tạo kết nối đến database dùng mysqli_connect()
